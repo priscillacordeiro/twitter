54e8e1039928f00a6fa35bee354bc0e028f6087c
Priscilla Cordeiro <priscillacordeiros@gmail.com>
2020-01-05 20:19:32 -0300

    adicionando atributo nome do usuario

--- src/io/github/priscillacordeiro/twitter/Usuario.java
+++ src/io/github/priscillacordeiro/twitter/Usuario.java
@@ -7,13 +7,15 @@ public class Usuario {
 	private String email;
 	private String senha;
 	private String nome;
+	private String nomeUsuario;
 	private Date dataNascimento;
 	private String bio;
 	
-	public Usuario(String email, String senha, String nome, Date dataNascimento, String bio) {
+	public Usuario(String email, String senha, String nome, String nomeUsuario, Date dataNascimento, String bio) {
 		this.email = email;
 		this.senha = senha;
 		this.nome = nome;
+		this.nomeUsuario = nomeUsuario;
 		this.dataNascimento = dataNascimento;
 		this.bio = bio;
 	}
@@ -41,6 +43,14 @@ public class Usuario {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
 
 	public Date getDataNascimento() {
 		return dataNascimento;
@@ -59,4 +69,3 @@ public class Usuario {
 	}
 	
 }
-
